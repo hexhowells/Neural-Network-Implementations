01e8861399e9d177f3363b7dc46984eedd7840f7
Morgan <58524460+hexhowells@users.noreply.github.com>
2021-10-02 17:00:12 +0100

    Removed old implementation from AlexNet.py
    AlexNet.py still had an older (and not in use) implementation in the code, this was removed.

--- models/alexnet.py
+++ models/alexnet.py
@@ -5,54 +5,6 @@ import numpy as np
 
 torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
-class AlexNet1(torch.nn.Module):
-	def __init__(self, n_class):
-		super(AlexNet, self).__init__()
-
-		self.conv1 = nn.Conv2d(3, 96, kernel_size=9, stride=3)
-		self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1)
-
-		self.conv2 = nn.Conv2d(96, 256, kernel_size=5)
-		self.pool2 = nn.MaxPool2d(kernel_size=2, stride=1)
-
-		self.conv3 = nn.Conv2d(256, 128, kernel_size=5)
-
-		#self.con4 = nn.Conv2d(384, 384, kernel_size=3)
-
-		self.conv5 = nn.Conv2d(128, 64, kernel_size=3)
-		self.pool5 = nn.MaxPool2d(kernel_size=3, stride=2)
-
-		self.linear1 = nn.Linear(576, 2048)
-		self.dropout1 = nn.Dropout(p=0.5)
-		self.linear2 = nn.Linear(2048, 2048)
-		self.dropout2 = nn.Dropout(p=0.5)
-		self.linear3 = nn.Linear(2048, n_class)
-
-
-	def forward(self, x):
-
-		x = F.relu(self.conv1(x))
-		x = F.relu(self.pool1(x))
-
-		x = F.relu(self.conv2(x))
-		x = F.relu(self.pool2(x))
-
-		x = F.relu(self.conv3(x))
-
-		#x = F.relu(self.con4(x))
-
-		x = F.relu(self.conv5(x))
-		x = F.relu(self.pool5(x))
-
-		x = x.view(-1, 576)
-		x = F.relu(self.linear1(x))
-		x = F.relu(self.dropout1(x))
-		x = F.relu(self.linear2(x))
-		x = F.relu(self.dropout2(x))
-		x = self.linear3(x)
-
-		return x.to(device='cpu')
-
 
 class AlexNet(nn.Module):
     def __init__(self, n_class):
